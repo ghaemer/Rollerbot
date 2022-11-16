f086fe05f69c17412652b33cf82a3e37796b7503
Krishna T <thankkrishna@gmail.com>
2022-11-15 21:16:32 -0800

    auton test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include "main.h"
+#include <string>
+#define startingv 50
 //test2
+
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::Motor topleft(3, false);
+	pros::Motor topright(7, false);	
+	pros::Motor bottleft(2, true);
+	pros::Motor bottright(1, true);
+	pros::Motor roller(13, false);
+	pros::Vision vision_sensor(16);
+	pros::Distance dist_sensor (8);
+
+
+	bool rollerbool = false;
+	int distance = 700;
+	int vision = 0;
+	int ymotion;
+	int xmotion;
 
 void initialize(){
 
@@ -12,18 +30,50 @@ void initialize(){
 
 }
 
+void autonomous() {
+
+	/*
+	topleft.move_velocity(startingv); //tells which motor to move at what voltage/direction
+	bottleft.move_velocity(startingv);
+	bottright.move_velocity(startingv);
+	topright.move_velocity(startingv);
+	pros::delay(2000);
+	*/
+	
+
+	//Checking for disk
+  	while (true)   {
+	distance = dist_sensor.get();
+	/*
+	topleft.move_velocity(startingv); //tells which motor to move at what voltage/direction
+	bottleft.move_velocity(startingv);
+	bottright.move_velocity(startingv);
+	topright.move_velocity(startingv);
+	*/
+
+    pros::vision_object_s_t color = vision_sensor.get_by_sig(0, 1);
+	pros::lcd::set_text(6, "Detected?: " + std::to_string(color.signature));
+	vision = color.signature;
+    
+	if (distance < 100 && color.signature == 1)	{
+		 pros::lcd::set_text(7, "detected ");
+		  break;
+		 }
+	}
+
+	pros::lcd::set_text(7, "Stopped ");
+	topleft.move_velocity(0); //tells which motor to move at what voltage/direction
+	bottleft.move_velocity(0);
+	bottright.move_velocity(0);
+	topright.move_velocity(0);
+	pros::delay(2);	
+
+
+}
+
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor topleft(3, false);
-	pros::Motor topright(7, false);	
-	pros::Motor bottleft(2, true);
-	pros::Motor bottright(1, true);
-	pros::Motor roller(13, false);
-
-	bool rollerbool = false;
-
-	int ymotion;
-	int xmotion;
+	
 
 	while (true) {
 
